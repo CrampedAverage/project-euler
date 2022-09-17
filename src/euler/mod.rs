3f5c0806ba623db1f9def09069a2f350870857e2
CrampedAverage <adnan.aant@gmail.com>
2022-09-18 00:04:13 +0100

    Completed euler problem 2

--- src/euler/mod.rs
+++ src/euler/mod.rs
@@ -1,6 +1,8 @@
 pub fn run() {
     euler_1();
+    euler_2();
 }
+
 fn euler_1() -> i32 {
     let mut i = 0;
     let mut total = 0;
@@ -15,13 +17,40 @@ fn euler_1() -> i32 {
     return total;
 }
 
+fn euler_2() -> i32 {
+    const LIMIT: i32 = 4_000_000;
+    let mut fibonacci = vec![];
+    fibonacci.push(1);
+    fibonacci.push(2);
+    let mut i = 2;
+    let mut total = 2;
+    while fibonacci[fibonacci.len() - 1] < LIMIT {
+        let value = fibonacci[i - 1] + fibonacci[i - 2];
+        if value % 2 == 0 {
+            total += &value;
+        }
+        fibonacci.push(value);
+        i += 1;
+    }
+    println!("{:?}", fibonacci);
+    println!("{}", total);
+    return total;
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::euler::euler_2;
+
     use super::euler_1;
 
     #[test]
     fn test_euler_1() {
         let result = euler_1();
         assert_eq!(result, 233168);
+    }
+    #[test]
+    fn test_euler_2() {
+        let result = euler_2();
+        assert_eq!(result, 4613732);
     }
 }
